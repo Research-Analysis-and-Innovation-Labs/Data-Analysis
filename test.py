ba5ab8e423359ba4a7ae6d5806912adf4a7b2e08
Sajal Tiwari <sajal.tiwari05@gmail.com>
2022-08-12 23:06:21 +0530

    test functions

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 def checkfunction(x):
     return x**2
 
-print(checkfunction(5))
+def test_checkfunction():
+    return checkfunction(2) == 4
+
+def test_checkfunction2():
+    return checkfunction(3) == 9
+
+print(test_checkfunction())
